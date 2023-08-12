bc9d1744dfef7356b5119d233233949698f77f0b
GlitchiPitch <141921165+GlitchiPitch@users.noreply.github.com>
2023-08-12 22:56:12 +0500

    add new power

--- src/server/Modules/ClayPowers.lua
+++ src/server/Modules/ClayPowers.lua
@@ -20,19 +20,71 @@ function ClayPowers.GetColorName(color)
     return 'black'
 end
 
-function ClayPowers:OnTouched(instance, callback)
-    instance.Touched:Connect(function(hitPart)
-        local character = hitPart:FindFirstChild('Humanoid').Parent
-        if not character then return end
-        callback()
+
+
+function ClayPowers.greenPower(instance)
+
+    local debrisLifeTime = 1
+
+    local debris = instance.Parent:FindFirstChild('Debris'):GetChildren()
+    local function getRandomDebris()
+        local currentDebris = debris[#debris]
+        table.remove(debris, table.find(debris, currentDebris, 1))
+        currentDebris.Touched:Connect(function(hitPart)
+            local humanoid = hitPart.Parent:FindFirstChild('Humanoid')
+            if not humanoid then return end
+            humanoid.Health -= 5
+        end)
+        return currentDebris
+    end
+
+    local function destroyDebris(debris)
+        debris.Color = instance.Color
+        debris.Anchored = false
+        task.wait(debrisLifeTime)
+        -- play sound acid burn
+        debris:Destory()
+
+    end
+
+    return coroutine.wrap(function()
+        while #debris > 0 do
+            destroyDebris(getRandomDebris())
+        end
+        instance:Destroy()
     end)
+
+
 end
 
 function ClayPowers.blackPower(instance)
+    
+    local subValue = .5
 
-    return ClayPowers:OnTouched(instance, function()
-        print('ok')
-    end)
+    local function getStrength()
+        local strength = (math.max(table.unpack({instance.Size.X, instance.Size.Y, instance.Size.Z})))
+        return strength
+    end
+    
+    instance:SetAttribute('Strength', getStrength())
+
+    return function(instance)
+        instance.Touched:Connect(function()
+            local x,y,z
+            instance.CanTouch = false
+            if instance.Size.X > subValue then x = subValue else x = 0 end 
+            if instance.Size.Y > subValue then y = subValue else y = 0 end
+            if instance.Size.Z > subValue then z = subValue else z = 0 end
+            instance.Size -= Vector3.new(x,y,z)
+            local tween = game:GetService('TweenService'):Create(instance, TweenInfo.new(2,Enum.EasingStyle.Bounce, Enum.EasingDirection.InOut, 10, true), {Size = instance.Size - Vector3.new(.5,.5,.5)})
+            tween:Play()
+            tween.Completed:Wait()
+            -- task.wait(2)
+            instance:SetAttribute('Strength', instance:GetAttribute('Strength') - 1)
+            instance.CanTouch = true
+            if instance:GetAttribute('Strength') <= 0 then instance:Destroy() end
+        end)
+    end
 end
 
 return ClayPowers
