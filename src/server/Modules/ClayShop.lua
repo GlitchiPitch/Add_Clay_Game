26b2aa52a8cb32cf88c10abaafefa299cc2c9e8c
GlitchiPitch <141921165+GlitchiPitch@users.noreply.github.com>
2023-08-15 23:04:16 +0500

    start setting shop Gui

--- src/server/Modules/ClayShop.lua
+++ src/server/Modules/ClayShop.lua
@@ -1,4 +1,6 @@
--- Working on client side
+-- Working on server side
+local PlayerSettings = require(game:GetService('ServerScriptService').Modules.PLayerSettings)
+
 local ClayShop = {}
 
 ClayShop.__index = ClayShop
@@ -13,8 +15,13 @@ end
 
 function ClayShop:Init()
     self.Button = self.Model:FindFirstChild('shopButton')
-    self.Button.Triggered:Connect(function()
-        
+    self.Button.Triggered:Connect(function(player)
+        local clayShopScreen = PlayerSettings.getClayShopScreen(player)
+        if clayShopScreen.Visible then
+            clayShopScreen.Visible = false
+        else
+            clayShopScreen.Visible = true
+        end
     end)
 end
 
